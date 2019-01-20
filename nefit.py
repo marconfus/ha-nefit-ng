50b25c97f70a26b112f402e0cb3f8b05da876bbf
Marco Reichwald <marconfus@gmail.com>
2019-01-20 16:02:08 +0100

    Invalid credential handling and cleaned up state attributes

--- nefit.py
+++ nefit.py
@@ -4,14 +4,15 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/xxxxxx/
 """
 
-REQUIREMENTS = ['aionefit==0.1']
+REQUIREMENTS = ['aionefit==0.3']
 
-import logging
-from datetime import timedelta
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
 import asyncio
 import concurrent
+from datetime import timedelta
+import homeassistant.helpers.config_validation as cv
+from homeassistant.exceptions import PlatformNotReady, InvalidStateError
+import logging
+import voluptuous as vol
 
 from homeassistant.components.climate import (ClimateDevice, PLATFORM_SCHEMA,
     STATE_AUTO, STATE_MANUAL, 
@@ -48,18 +49,9 @@ async def async_setup_platform(hass, config, async_add_entities,
 
     _LOGGER.debug("Creating device")
     device = NefitThermostat(hass, name, serial, accesskey, password)
-    _LOGGER.debug("Waiting for connected event")
-    
-    try:
-        await device._client.xmppclient.connected_event.wait()
-    except concurrent.futures._base.TimeoutError:
-        hass.services.call("persistent_notification", "create", 
-                           {'message': 'Could not connect to Bosch cloud.',
-                           'title': 'Nefit error',
-                           'notification_id': 'nefit_logon_error'})
-
-    _LOGGER.debug("Device is ready")
+    await device.connect()
     async_add_entities([device], True)
+    _LOGGER.debug("async_setup_platform done")
 
 class NefitThermostat(ClimateDevice):
     """Representation of a NefitThermostat device."""
@@ -70,20 +62,48 @@ class NefitThermostat(ClimateDevice):
         self.hass = hass
         self._name = name
 
+        self.error_state = "initializing"
         self._online = False
         self._unit_of_measurement = TEMP_CELSIUS
         self._uistatus = None
         self._attributes = {}
+        self._stateattr = {}
+        self._data = {}
         self._operation_list = [OPERATION_MANUAL, OPERATION_AUTO]
 
         self._client = NefitCore(serial_number=serial,
                        access_key=accesskey,
                        password=password,
                        message_callback=self.parse_message)
+        self._client.failed_auth_handler=self.failed_auth_handler
 
-        _LOGGER.debug("adding stop listener")
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP,
-                                   self._shutdown)
+    async def connect(self):
+        self._client.connect()
+        _LOGGER.debug("Waiting for connected event")        
+        try:
+            # await self._client.xmppclient.connected_event.wait()
+            await asyncio.wait_for(self._client.xmppclient.connected_event.wait(), timeout=5.0)
+            _LOGGER.debug("adding stop listener")
+            self.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP,
+                                            self._shutdown)
+        except concurrent.futures._base.TimeoutError:
+            _LOGGER.debug("TimeoutError on waiting for connected event")
+            self.hass.components.persistent_notification.create( 
+                'Timeout while connecting to Bosch cloud. Retrying in the background',
+                title='Nefit error',
+                notification_id='nefit_logon_error')
+            raise PlatformNotReady
+        
+        if self.error_state == "authentication_failed":
+            self.hass.components.persistent_notification.create( 
+                'Invalid credentials while connecting to Bosch cloud.',
+                title='Nefit error',
+                notification_id='nefit_logon_error')
+            raise PlatformNotReady
+
+    def failed_auth_handler(self, event):
+        self.error_state = "authentication_failed"
+        self._client.xmppclient.connected_event.set()
 
     @property
     def supported_features(self):
@@ -91,23 +111,42 @@ class NefitThermostat(ClimateDevice):
         """
         return SUPPORT_FLAGS
 
+    @property
+    def target_temperature_step(self):
+        return 0.5
+
     def parse_message(self, data):
         """Message received callback function for the XMPP client.
         """
-        _LOGGER.debug("parse_message callback called")
+        _LOGGER.debug("parse_message callback called with data %s", data)
         if not 'id' in data:
-            _LOGGER.error("Unknown response received.")
+            _LOGGER.error("Unknown response received: %s", data)
             return
 
         if data['id'] == '/ecus/rrc/uiStatus':
-            _LOGGER.debug('Message type uiStatus')
             self._uistatus = data['value']
+            self._data['temp_setpoint'] = float(data['value']['TSP'])
+            self._data['inhouse_temperature'] = float(data['value']['IHT'])
+            self._data['user_mode'] = data['value']['UMD']
+            self._stateattr['current_time'] = data['value']['CTD']
 
+        if data['id'] == '/heatingCircuits/hc1/actualSupplyTemperature':
+            self._stateattr['supply_temperature'] = data['value']
+
+        if data['id'] == '/system/appliance/systemPressure':
+            self._stateattr['system_pressure'] = data['value']
+            
+        if data['id'] == '/ecus/rrc/recordings/yearTotal':
+            self._stateattr['year_total'] = data['value']
+            
     async def async_update(self):
         """Get latest data
         """
         _LOGGER.debug("async_update called")
         self._client.get('/ecus/rrc/uiStatus')
+        self._client.get('/heatingCircuits/hc1/actualSupplyTemperature')
+        self._client.get('/system/appliance/systemPressure')
+        self._client.get('/ecus/rrc/recordings/yearTotal')
 
         await asyncio.wait_for(self._client.xmppclient.message_event.wait(), timeout=10.0)
         self._client.xmppclient.message_event.clear()
@@ -129,21 +168,17 @@ class NefitThermostat(ClimateDevice):
     def current_temperature(self):
         """Return the current temperature.
         """
-        if not self._uistatus:
-            return STATE_UNKNOWN
-
-        iht = float(self._uistatus.get('IHT'))
-        _LOGGER.debug('current_temperature=%s', iht)
-        return iht
+        if 'inhouse_temperature' in self._data:
+            return self._data.get('inhouse_temperature')
+        else:
+            return 0
 
     @property
     def target_temperature(self):
-        if not self._uistatus:
-            return STATE_UNKNOWN
-
-        tot = float(self._uistatus.get('TOT'))
-        _LOGGER.debug('target_temperature=%s', tot)
-        return tot
+        if 'temp_setpoint' in self._data:
+            return self._data.get('temp_setpoint')
+        else:
+            return 0
 
     @property
     def operation_list(self):
@@ -152,19 +187,18 @@ class NefitThermostat(ClimateDevice):
 
     @property
     def current_operation(self):
-        if not self._uistatus:
-            return None
-
-        if self._uistatus['UMD'] == 'manual':
+        if self._data.get('user_mode') == 'manual':
             return OPERATION_MANUAL
-        elif self._uistatus['UMD'] == 'clock':
+        elif self._data.get('user_mode') == 'clock':
             return OPERATION_AUTO
+        else:
+            return STATE_UNKNOWN
 
     @property
     def device_state_attributes(self):
         """Return the device specific state attributes."""
 
-        return self._uistatus
+        return self._stateattr
 
     async def async_set_operation_mode(self, operation_mode):
         """Set new target operation mode."""
@@ -177,7 +211,7 @@ class NefitThermostat(ClimateDevice):
         self._client.set_usermode(new_mode)
         await asyncio.wait_for(self._client.xmppclient.message_event.wait(), timeout=10.0)
         self._client.xmppclient.message_event.clear()
-        self._uistatus['UMD'] = new_mode
+        self._data['user_mode'] = new_mode
 
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
@@ -186,7 +220,8 @@ class NefitThermostat(ClimateDevice):
         self._client.set_temperature(temperature)
         await asyncio.wait_for(self._client.xmppclient.message_event.wait(), timeout=10.0)
         self._client.xmppclient.message_event.clear()
-        self._uistatus['TOT'] = temperature
+        self._data['target_temperature'] = temperature
+        
 
     def _shutdown(self, event):
         _LOGGER.debug("shutdown")
